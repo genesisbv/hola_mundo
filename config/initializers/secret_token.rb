06e9eda5f9d7bd4fc5db98476278dfa650e35f21
genesisbv <genesisb.264@gmail.com>
2014-04-01 08:12:39 -0430

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HolaMundo::Application.config.secret_key_base = '87d48157b965a9c7067bbe96dcd9b9c0d2d0351c38f5b72edf2578f82db534b52540038ebbb772480a1b6d93bbe1f8bf30ad5667593e32997ea1d1e815781e92'
